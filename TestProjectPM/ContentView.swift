6ef48d02f7ecdf6bbfd862ca755054ea2983eb2c
jefferymason <jefferymason72@gmail.com>
2021-01-20 12:32:20 -0600

    Changed hello to hey.

---
+++ TestProjectPM/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestProjectPM
+//
+//  Created by Jeff Mason on 1/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hey")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestProjectPM/TestProjectPMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectPMApp.swift
+//  TestProjectPM
+//
+//  Created by Jeff Mason on 1/20/21.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectPMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
